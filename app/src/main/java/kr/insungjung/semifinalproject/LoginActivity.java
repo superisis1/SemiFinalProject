e11ee804ca6f998a9958125f368eb9aa864b338e
Insung Jung <superisis@gmail.com>
2019-05-14 10:01:45 +0900

    코드 정리

--- app/src/main/java/kr/insungjung/semifinalproject/LoginActivity.java
+++ app/src/main/java/kr/insungjung/semifinalproject/LoginActivity.java
@@ -1,12 +1,9 @@
 package kr.insungjung.semifinalproject;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 
 import org.json.JSONException;
@@ -35,15 +32,16 @@ public class LoginActivity extends BaseActivity {
         act.signInBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                1. 아이디와 비번을 뭐라고 입력했는지? 받아오기
+
+                // 1. 아이디와 비번을 뭐라고 입력했는지? 받아오기
                 String inputId = act.loginIdEdt.getText().toString();
                 String inputPw = act.loginPwEdt.getText().toString();
 
-//                1.2 - 입력받은 ID를 SharedPreferences에 저장.
+                // 1.2 - 입력받은 ID를 SharedPreferences 에 저장.
                 ContextUtil.setUserInputId(mContext, inputId);
 
-//                2. 받아온 아이디와 비번이 정말로 올바른 회원지? 검사
-//                 아이디/비번이 모두 동일한 사람이 회원 명부에 있는지? 검사.
+                // 2. 받아온 아이디와 비번이 정말로 올바른 회원지? 검사
+                //    아이디/비번이 모두 동일한 사람이 회원 명부에 있는지? 검사.
                 ConnectServer.postRequestSignIn(mContext, inputId, inputPw, new ConnectServer.JsonResponseHandler() {
                     @Override
                     public void onResponse(JSONObject json) {
@@ -53,32 +51,24 @@ public class LoginActivity extends BaseActivity {
                             public void run() {
 
                                 try {
+
                                     int code = json.getInt("code");
 
-                                    if (code == 200) {
-//                                        로그인 성공!
+                                    if (code == 200) { // 로그인 성공!
 
                                         JSONObject data = json.getJSONObject("data");
                                         String token = data.getString("token");
 
-                                        if (act.autoLoginCheckBox.isChecked()) {
-//                                          자동로그인을 하려고 한다. 사용자가 표시
-//                                            로그인 성공 토큰값을 SharedPreference에 저장
+                                        if (act.autoLoginCheckBox.isChecked()) {  // 자동로그인을 하려고 한다. 사용자가 표시
 
-                                            ContextUtil.setUserToken(mContext, token);
-
+                                            ContextUtil.setUserToken(mContext, token); // 로그인성공 토큰값을 SharedPreferences 에 저장
                                         }
 
                                         Intent intent = new Intent(mContext, MainActivity.class);
                                         intent.putExtra("userToken", token);
                                         startActivity(intent);
                                         finish();
-
-
-
-                                    }
-                                    else {
-//                                        로그인 실패. 왜 실패했는지 AlertDialog
+                                    } else { // 로그인 실패. 왜 실패했는지 AlertDialog
 
                                         AlertDialog.Builder alert = new AlertDialog.Builder(mContext);
                                         alert.setTitle("로그인 실패 알림");
@@ -88,20 +78,16 @@ public class LoginActivity extends BaseActivity {
                                     }
 
                                 } catch (JSONException e) {
+
                                     e.printStackTrace();
                                 }
-
                             }
                         });
 
                     }
                 });
-                /*
-                test123 / Test!123
-                iu0001 / Test!123
-                testorder1 / testorder1
-                gggg1111 / gggg1111
-                */
+
+                // test123,Test!123 / iu0001,Test!123 / testorder1,testorder1 / gggg1111,gggg1111
             }
         });
     }
--- app/src/main/java/kr/insungjung/semifinalproject/utils/ContextUtil.java
+++ app/src/main/java/kr/insungjung/semifinalproject/utils/ContextUtil.java
@@ -12,45 +12,43 @@ public class ContextUtil {
     private static final String USER_INPUT_ID = "USER_INPUT_ID";
     private static final String USER_TOKEN = "USER_TOKEN";
 
-//    실제로 각각의 항목을 저장/불러오는 기능
-//    setter : ID를 저장하는 id_setter
+
+    /**
+     * 아이디 저장
+     * 실제로 각각의 항목을 저장/불러오는 기능
+     * @param context
+     * @param inputId
+     */
+    // setter : ID를 저장하는 id_setter
     public static void setUserInputId(Context context, String inputId) {
 
-//        메모장 파일 (TXT) 을 여는 작업.
-        SharedPreferences pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE);
-
-//        실제로 데이터 저장하기
-        pref.edit().putString(USER_INPUT_ID, inputId).apply();
-
+        SharedPreferences pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE); // 메모장파일(TXT) 여는 작업
+        pref.edit().putString(USER_INPUT_ID, inputId).apply(); // 실제로 데이터 저장하기
     }
 
-//    getter : 저장된 ID가 있다면, 불러오기.
+    // getter : 저장된 ID가 있다면, 불러오기
     public static String getUserInputId(Context context) {
 
-//        메모장 파일을 열어주는 작업.
-        SharedPreferences pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE);
-
+        SharedPreferences pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE); // 메모장 파일을 열어주는 작업
         return pref.getString(USER_INPUT_ID, "");
     }
 
 
-//    setter : 토큰값을 저장하는 token_setter
+    /**
+     * 토큰 저장
+     * @param context
+     * @param token
+     */
+
     public static void setUserToken(Context context, String token) {
 
-//        메모장 파일 (TXT) 을 여는 작업.
         SharedPreferences pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE);
-
-//        실제로 데이터 저장하기
         pref.edit().putString(USER_TOKEN, token).apply();
-
     }
 
-//    getter : 저장된 ID가 있다면, 불러오기.
     public static String getUserToken(Context context) {
 
-//        메모장 파일을 열어주는 작업.
         SharedPreferences pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE);
-
         return pref.getString(USER_TOKEN, "");
     }
 }
